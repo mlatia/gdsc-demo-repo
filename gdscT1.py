701e859854765e6e280ab9f52db8388c036ee5ef
mlatia <112107340+mlatia@users.noreply.github.com>
2022-11-18 21:28:49 +0700

    initial commit

---
+++ gdscT1.py
@@ -0,0 +1,4 @@
+print('Nama : Melati Anggraini')
+print('Jurusan : STEI-K')
+print('Universitas : Institut Teknologi Bandung')
+print('Alasan ingin masuk GDSC : Ingin mengembangkan minat di dunia teknologi, terutama di bidang data science dan web development, serta menambah relasi dengan orang-orang yang memiliki minat yang sama.')
